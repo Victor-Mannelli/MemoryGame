e3c7c43292383f0d754e585affdc6f87d05e00a5
Victor Mannelli <imp.mannelli@gmail.com>
2022-08-20 23:23:55 -0300

    fix: working turning cards

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 
-const ul = document.querySelector('ul')
+const main = document.querySelector('main')
+
 let numberOfCards = Number(prompt('How many cards do you want?'))
-counter = 0;
+let counter = 0;
+
+let images = ["banana", "berry", "kiwi", "orange", "strawberry", "watermelon"];
+images.sort(scramble);
+function scramble(){
+    return Math.random() - 0.5; 
+}
 
 addingCards()
 function addingCards(){
-    
+
     while (isNaN(numberOfCards) || numberOfCards > 14 || numberOfCards < 4 || numberOfCards % 2 !== 0){
         numberOfCards = Number(prompt('How many cards do you want? It has to be even and beetween 4 and 14!'))
     }
 
     for(let i = 0; i < numberOfCards; i++){
         
-        ul.innerHTML += `
-        <li onclick="turningCards(this)"> 
-            <img src="./archives/parrot.png"></img>
-        </li>
+        main.innerHTML += `
+        <div class="card" onclick="turningCards(this)"> 
+            <div class="card-inner">
+                <div class="flip-card-front">
+                    <img class="parrot" src="./archives/parrot.png" alt=""></img>
+                </div>
+                <div class="flip-card-back">
+                    <img class="${images[i]}" src="./archives/${images[i]}.png" alt=""></img>
+                </div>
+            </div>
+        </div>
         `;
     }
 }
 
 function turningCards(element){
-    if (element.classList.contains('cardBack')){
-        element.classList.remove('cardBack');
+    console.log(element);
+    if (element.classList.contains('flippedCard')){
+        element.classList.remove('flippedCard');
         counter--;
     } else {
-        element.classList.add('cardBack');
+        element.classList.add('flippedCard');
         counter++;
     }    
+    console.log(counter)
 }
 
 function maxNumberOfTurnedCards(){
